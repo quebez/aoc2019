c9c09852d2911ef3715c9ccc2b46b9f94269b1b8
quebez <marty.janik@gmail.com>
2019-12-18 23:18:52 +0100

    7 done

--- 7 Amplification Circuit.js
+++ 7 Amplification Circuit.js
@@ -1,136 +1,18 @@
 'use strict'
 
-require('./intcode.js');
+var intcode = require('./intcode.js');
 let fs = require('fs');
 let file = './7.txt';
 let input = fs.readFileSync(file, 'utf8').split(',').map(v => parseInt(v));
 //let input = '3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0'.split(',').map(v => parseInt(v));
+//let input = '3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10'.split(',').map(v => parseInt(v));
+//let input = '3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5'.split(',').map(v => parseInt(v));
 
 
 /************* FIRST TASK *************/
 let first = (arr) => {
   let res = [];
-
-  let combos = [[0, 1, 2, 3, 4],
-  [0, 1, 2, 4, 3],
-  [0, 1, 3, 4, 2],
-  [0, 1, 3, 2, 4],
-  [0, 1, 4, 2, 3],
-  [0, 1, 4, 3, 2],
-  [0, 2, 0, 3, 4],
-  [0, 2, 0, 4, 3],
-  [0, 2, 3, 4, 0],
-  [0, 2, 3, 0, 4],
-  [0, 2, 4, 0, 3],
-  [0, 2, 4, 3, 0],
-  [0, 3, 0, 2, 4],
-  [0, 3, 0, 4, 2],
-  [0, 3, 2, 4, 0],
-  [0, 3, 2, 0, 4],
-  [0, 3, 4, 0, 2],
-  [0, 3, 4, 2, 0],
-  [0, 4, 0, 2, 3],
-  [0, 4, 0, 3, 2],
-  [0, 4, 2, 3, 0],
-  [0, 4, 2, 0, 3],
-  [0, 4, 3, 0, 2],
-  [0, 4, 3, 2, 0],
-  [1, 0, 2, 3, 4],
-  [1, 0, 2, 4, 3],
-  [1, 0, 3, 4, 2],
-  [1, 0, 3, 2, 4],
-  [1, 0, 4, 2, 3],
-  [1, 0, 4, 3, 2],
-  [1, 2, 0, 3, 4],
-  [1, 2, 0, 4, 3],
-  [1, 2, 3, 4, 0],
-  [1, 2, 3, 0, 4],
-  [1, 2, 4, 0, 3],
-  [1, 2, 4, 3, 0],
-  [1, 3, 0, 2, 4],
-  [1, 3, 0, 4, 2],
-  [1, 3, 2, 4, 0],
-  [1, 3, 2, 0, 4],
-  [1, 3, 4, 0, 2],
-  [1, 3, 4, 2, 0],
-  [1, 4, 0, 2, 3],
-  [1, 4, 0, 3, 2],
-  [1, 4, 2, 3, 0],
-  [1, 4, 2, 0, 3],
-  [1, 4, 3, 0, 2],
-  [1, 4, 3, 2, 0],
-  [2, 0, 1, 3, 4],
-  [2, 0, 1, 4, 3],
-  [2, 0, 3, 4, 1],
-  [2, 0, 3, 1, 4],
-  [2, 0, 4, 1, 3],
-  [2, 0, 4, 3, 1],
-  [2, 1, 0, 3, 4],
-  [2, 1, 0, 4, 3],
-  [2, 1, 3, 4, 0],
-  [2, 1, 3, 0, 4],
-  [2, 1, 4, 0, 3],
-  [2, 1, 4, 3, 0],
-  [2, 3, 0, 1, 4],
-  [2, 3, 0, 4, 1],
-  [2, 3, 1, 4, 0],
-  [2, 3, 1, 0, 4],
-  [2, 3, 4, 0, 1],
-  [2, 3, 4, 1, 0],
-  [2, 4, 0, 1, 3],
-  [2, 4, 0, 3, 1],
-  [2, 4, 1, 3, 0],
-  [2, 4, 1, 0, 3],
-  [2, 4, 3, 0, 1],
-  [2, 4, 3, 1, 0],
-  [3, 0, 1, 2, 4],
-  [3, 0, 1, 4, 2],
-  [3, 0, 2, 4, 1],
-  [3, 0, 2, 1, 4],
-  [3, 0, 4, 1, 2],
-  [3, 0, 4, 2, 1],
-  [3, 1, 0, 2, 4],
-  [3, 1, 0, 4, 2],
-  [3, 1, 2, 4, 0],
-  [3, 1, 2, 0, 4],
-  [3, 1, 4, 0, 2],
-  [3, 1, 4, 2, 0],
-  [3, 2, 0, 1, 4],
-  [3, 2, 0, 4, 1],
-  [3, 2, 1, 4, 0],
-  [3, 2, 1, 0, 4],
-  [3, 2, 4, 0, 1],
-  [3, 2, 4, 1, 0],
-  [3, 4, 0, 1, 2],
-  [3, 4, 0, 2, 1],
-  [3, 4, 1, 2, 0],
-  [3, 4, 1, 0, 2],
-  [3, 4, 2, 0, 1],
-  [3, 4, 2, 1, 0],
-  [4, 0, 1, 2, 3],
-  [4, 0, 1, 3, 2],
-  [4, 0, 2, 3, 1],
-  [4, 0, 2, 1, 3],
-  [4, 0, 3, 1, 2],
-  [4, 0, 3, 2, 1],
-  [4, 1, 0, 2, 3],
-  [4, 1, 0, 3, 2],
-  [4, 1, 2, 3, 0],
-  [4, 1, 2, 0, 3],
-  [4, 1, 3, 0, 2],
-  [4, 1, 3, 2, 0],
-  [4, 2, 0, 1, 3],
-  [4, 2, 0, 3, 1],
-  [4, 2, 1, 3, 0],
-  [4, 2, 1, 0, 3],
-  [4, 2, 3, 0, 1],
-  [4, 2, 3, 1, 0],
-  [4, 3, 0, 1, 2],
-  [4, 3, 0, 2, 1],
-  [4, 3, 1, 2, 0],
-  [4, 3, 1, 0, 2],
-  [4, 3, 2, 0, 1],
-  [4, 3, 2, 1, 0]];
+  let combos = permute([0, 1, 2, 3, 4]);
 
   for (let i = 0; i < combos.length; i++) {
     res.push(intcode5times(arr, combos[i]));
@@ -141,25 +23,86 @@ let first = (arr) => {
 
 /* Helpers for 1st task */
 let intcode5times = (arr, inputs) => {
-  let a = intcode(arr, [inputs[0], 0]);
-  let b = intcode(arr, [inputs[1], a]);
-  let c = intcode(arr, [inputs[2], b]);
-  let d = intcode(arr, [inputs[3], c]);
-  return intcode(arr, [inputs[4], d]);
+  let carry = 0;
+
+  for (let i = 0; i < inputs.length; i++) {
+    let pc = intcode.compute(arr);
+    pc.next();
+    pc.next(inputs[i]);
+    carry = pc.next(carry).value;
+  }
+
+  return carry;
+}
+
+let permute = permutation => {
+  let length = permutation.length,
+    result = [permutation.slice()],
+    c = new Array(length).fill(0),
+    i = 1, k, p;
+
+  while (i < length) {
+    if (c[i] < i) {
+      k = i % 2 && c[i];
+      p = permutation[i];
+      permutation[i] = permutation[k];
+      permutation[k] = p;
+      ++c[i];
+      i = 1;
+      result.push(permutation.slice());
+    } else {
+      c[i] = 0;
+      ++i;
+    }
+  }
+  return result;
 }
 
 
 /************* SECOND TASK *************/
 let second = (arr) => {
+  let res = [];
+  let phaseSettings = permute([5, 6, 7, 8, 9]);
 
+  for (let i = 0; i < phaseSettings.length; i++) {
+    res.push(compute(arr.slice(), phaseSettings[i]));
+  }
+
+  return Math.max(...res);
 }
 
 /* Helpers for 2nd task */
+function generator(arr, phaseSetting) {
+  let pc = intcode.compute(arr);
+  pc.next();
+  pc.next(phaseSetting);
+  return pc;
+}
 
+let compute = (arr, phaseSettings) => {
+  let first = generator(arr.slice(), phaseSettings[0]);
+  let second = generator(arr.slice(), phaseSettings[1]);
+  let third = generator(arr.slice(), phaseSettings[2]);
+  let fourth = generator(arr.slice(), phaseSettings[3]);
+  let fifth = generator(arr.slice(), phaseSettings[4]);
+  let generators = [first, second, third, fourth, fifth];
+  let carry = 0;
+  let completed = false;
+
+  while (!completed) {
+    for (let i = 0; i < generators.length; i++) {
+      let res = generators[i].next(carry);
+      generators[i].next(phaseSettings[i]);
+
+      if (res.done == true) return carry;
+      else carry = res.value;
+    }
+  }
+}
 
 
 /* OUT */
-console.log(`First answer: ${first(input.slice())}.`);
+//console.log(`First answer: ${first(input.slice())}. Correct is 14902.`);
 console.log(`Second answer: ${second(input.slice())}.`);
 
 /*
--- intcode.js
+++ intcode.js
@@ -2,9 +2,8 @@
 iProgram: An Intcode program is a list of integers.
 input: a single integer.
 */
-module.exports = intcode = (iProgram, input = []) => {
+module.exports.compute = function* (iProgram) {
   let output = null;
-  let inputIndex = 0;
 
   for (let i = 0; i < iProgram.length; i) {
     // Parse opcode to opcode and mode position(0) (default) || immediate(1)
@@ -33,13 +32,13 @@ module.exports = intcode = (iProgram, input = []) => {
         break;
       /* Opcode 3 takes a single integer as input and saves it to the position given by its only parameter. For example, the instruction 3,50 would take an input value and store it at address 50. */
       case 3:
-        iProgram[iProgram[i + 1]] = input[inputIndex++];
+        iProgram[iProgram[i + 1]] = yield;
         i += 2;
         break;
       /* Opcode 4 outputs the value of its only parameter. For example, the instruction 4,50 would output the value at address 50. */
       case 4:
-        //console.log(`Requested output: ${a}`);
-        output = a;
+        // console.log(`Requested output: ${a}`);
+        yield a;
         i += 2;
         break;
       /* Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing. */
@@ -64,7 +63,8 @@ module.exports = intcode = (iProgram, input = []) => {
         break;
       /* Opcode 99 halts the program. No parameters. */
       case 99:
-        return output || output == 0 ? output : iProgram;
+        //console.log(output);
+        return output;// || output == 0 ? output : iProgram;
       default:
         throw new Error(`You've fucked up. No code defined for code ${opcode}.`);
     }
